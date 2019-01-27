9dfaa2813d5bbd9e90ad2635a4fd0faf7f44de47
hunmod <hunmod@gmail.com>
2019-01-27 22:51:23 +0100

    no message

--- items/cars/web/list.php
+++ items/cars/web/list.php
@@ -7,6 +7,58 @@ font-size:23px;
 }
 
 </style>
+<script type="text/javascript">
+    (function() {
+
+        /* matchHeight example */
+
+        $(function() {
+            // apply your matchHeight on DOM ready (they will be automatically re-applied on load or resize)
+
+            // get test settings
+            var byRow = $('body').hasClass('test-rows');
+
+            // apply matchHeight to each item container's items
+            $('.items-container').each(function() {
+                $(this).children('.item').matchHeight({
+                    byRow: byRow
+                });
+            });
+
+            // test target
+            $('.target-items').each(function() {
+                $(this).children('.item-0, .item-2, .item-3').matchHeight({
+                    target: $(this).find('.item-1')
+                });
+            });
+
+            // example of removing matchHeight
+            $('.btn-remove').click(function() {
+                $('.item').matchHeight({
+                    remove: true
+                });
+            });
+
+            // button to show hidden elements
+            $('.btn-hidden').click(function() {
+                $('body').removeClass('test-hidden');
+            });
+
+            // example of update callbacks (uncomment to test)
+            $.fn.matchHeight._beforeUpdate = function(event, groups) {
+                //var eventType = event ? event.type + ' event, ' : '';
+                //console.log("beforeUpdate, " + eventType + groups.length + " groups");
+            }
+
+            $.fn.matchHeight._afterUpdate = function(event, groups) {
+                //var eventType = event ? event.type + ' event, ' : '';
+                //console.log("afterUpdate, " + eventType + groups.length + " groups");
+            }
+        });
+
+    })();
+</script>
+
 <div class="container">
             <!--div id="breadCrumb">
                 <a href="<?php echo $homeurl;?>">Home</a> / 
--- items/hirek/web/hir_display_block2.php
+++ items/hirek/web/hir_display_block2.php
@@ -33,8 +33,7 @@ $elem["url"]=$hir_class->createurl($elem);
 ;?>" alt="<?php echo $Text_Class->htmlfromchars($elem["cim"]);?>"  title="<?php echo $Text_Class->htmlfromchars($elem["cim"]);?>"  >
                             </div>
                             <div class="upArrowDecor"></div>
-                            <h2 itemprop="name"><?php echo $Text_Class->htmlfromchars($elem["cim"]);?></h2>
-							<p itemprop="description"><?php echo substr($Text_Class->tageketcsupaszit($Text_Class->htmlfromchars($elem["hir"])),0,130).'...';?></p>
+                            <span class="namefield" itemprop="name"><?php echo $Text_Class->htmlfromchars($elem["cim"]);?></span>
 
                         </a>
                     </car>
