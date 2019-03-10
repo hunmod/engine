eda84ab11b498dcac7f3e0b73ceeed7eab4b582b
hunmod <hunmod@gmail.com>
2019-03-10 18:55:19 +0100

    no message

---
+++ items/cars/web/order.php
@@ -0,0 +1,12 @@
+
+<div class="container">
+    <section>
+<?= $Text_Class->htmlfromchars( page_settings('footerblock4_hu'));?>
+        <form>
+
+
+
+        </form>
+    </section>
+    <div class="clear"></div>
+</div>
