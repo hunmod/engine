bfed3dcb422f6b77336908f6261a70cac2312ab4
hunmod <hunmod@gmail.com>
2019-03-13 23:08:48 +0100

    no message

--- items/cars/web/order.php
+++ items/cars/web/order.php
@@ -3,9 +3,45 @@
     <section>
 <?= $Text_Class->htmlfromchars( page_settings('footerblock4_hu'));?>
         <form>
+    <div class="col-sm-12">
+<?=lan('megrendelodatai')?>
+    </div>
+            <div class="col-sm-12">
+                <?php $form->textbox("nev", $Text_Class->htmlfromchars($adat["nev"]),lan('nev')) ?>
+            </div>
+            <div class="col-sm-12">
+                <?php $form->textbox("email", $Text_Class->htmlfromchars($adat["email"]),lan('email')) ?>
+            </div>
+            <div class="col-sm-12">
+                <?php $form->textbox("tel", $Text_Class->htmlfromchars($adat["tel"]),lan('tel')) ?>
+            </div>
+            <div class="col-sm-12">
+                <?php $form->textbox("cim", $Text_Class->htmlfromchars($adat["cim"]),lan('cim')) ?>
+            </div>
+            <div class="col-sm-12">
+                <?php $form->textbox("carid", $Text_Class->htmlfromchars($adat["carid"])) ?>
+            </div>
+            <div class="col-sm-12">
+                <?php $form->datebox('ido',$adat["ido"],lan('idopont')) ?>
+            </div>
+            <div class="col-sm-12">
+            <?php for ($c = 1; $c <= 10; $c++) { ?>
+                <div class="col-sm-12">
+                    <div class="col-sm-3 dt1" >
+                        <?php $form->textbox("ora_".$c, $Text_Class->htmlfromchars($adat["ora_".$c])); ?>
+                    </div>
+                    <div class="col-sm-3" >
+                        <?php $form->textbox("perc_".$c, $Text_Class->htmlfromchars($adat["perc_".$c])); ?>
+                    </div>
+                    <div class="col-sm-6" >
+                        <?php $form->textbox("cim_".$c, $Text_Class->htmlfromchars($adat["cim_".$c])); ?>
+                    </div>
 
+                </div>
 
-
+                <?php } ?>
+            </div>
+            <submit class="Button" >save</submit>
         </form>
     </section>
     <div class="clear"></div>
