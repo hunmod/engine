215c4abfd1db9f85598288f004fda2cbc3e2da13
hunmod <hunmod@gmail.com>
2019-03-13 23:19:47 +0100

    no message

--- items/cars/web/order.php
+++ items/cars/web/order.php
@@ -8,41 +8,41 @@ if ($filterc['id']>0) {
 <div class="container">
     <section>
 <?= $Text_Class->htmlfromchars( page_settings('footerblock4_hu'));?>
-        <form>
+        <form method="post">
     <div class="col-sm-12">
 <?=lan('megrendelodatai')?>
     </div>
             <div class="col-sm-12">
-                <?php $form->textbox("nev", $Text_Class->htmlfromchars($adat["nev"]),lan('nev')) ?>
+                <?php $Form_Class->textbox("nev", $Text_Class->htmlfromchars($adat["nev"]),lan('nev')) ?>
             </div>
             <div class="col-sm-12">
-                <?php $form->textbox("email", $Text_Class->htmlfromchars($adat["email"]),lan('email')) ?>
+                <?php $Form_Class->textbox("email", $Text_Class->htmlfromchars($adat["email"]),lan('email')) ?>
             </div>
             <div class="col-sm-12">
-                <?php $form->textbox("tel", $Text_Class->htmlfromchars($adat["tel"]),lan('tel')) ?>
+                <?php $Form_Class->textbox("tel", $Text_Class->htmlfromchars($adat["tel"]),lan('tel')) ?>
             </div>
             <div class="col-sm-12">
-                <?php $form->textbox("cim", $Text_Class->htmlfromchars($adat["cim"]),lan('cim')) ?>
+                <?php $Form_Class->textbox("cim", $Text_Class->htmlfromchars($adat["cim"]),lan('cim')) ?>
             </div>
             <div class="col-sm-12">
-                <?php $form->textbox("carid", $Text_Class->htmlfromchars($adat["carid"]));
+                <?php $Form_Class->textbox("carid", $Text_Class->htmlfromchars($adat["carid"]));
                 arraylist($car_data);
                 ?>
             </div>
             <div class="col-sm-12">
-                <?php $form->datebox('ido',$adat["ido"],lan('idopont')) ?>
+                <?php $Form_Class->datebox('ido',$adat["ido"],lan('idopont')) ?>
             </div>
             <div class="col-sm-12">
             <?php for ($c = 1; $c <= 10; $c++) { ?>
                 <div class="col-sm-12">
                     <div class="col-sm-3 dt1" >
-                        <?php $form->textbox("ora_".$c, $Text_Class->htmlfromchars($adat["ora_".$c])); ?>
+                        <?php $Form_Class->textbox("ora_".$c, $Text_Class->htmlfromchars($adat["ora_".$c])); ?>
                     </div>
                     <div class="col-sm-3" >
-                        <?php $form->textbox("perc_".$c, $Text_Class->htmlfromchars($adat["perc_".$c])); ?>
+                        <?php $Form_Class->textbox("perc_".$c, $Text_Class->htmlfromchars($adat["perc_".$c])); ?>
                     </div>
                     <div class="col-sm-6" >
-                        <?php $form->textbox("cim_".$c, $Text_Class->htmlfromchars($adat["cim_".$c])); ?>
+                        <?php $Form_Class->textbox("cim_".$c, $Text_Class->htmlfromchars($adat["cim_".$c])); ?>
                     </div>
 
                 </div>
