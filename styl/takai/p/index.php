8f051158e46cd0d6732d9fb0f47aed84aced8657
hunmod <hunmod@gmail.com>
2020-12-06 18:21:14 +0100

    parlaxcss?

---
+++ styl/takai/p/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta name="author" content="Frontendscript" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="videoparallax.css" />
+</head>
+<body>
+
+
+<div class="ParalaxPage">
+
+
+     <div class="ParallaxVideo">
+        <video autoplay muted loop>
+            <source src="fire.mp4" type="video/mp4">
+        </video>
+         <div class="container">
+             <h1>#Taijaki:</h1>
+             <p>Egy tengeri keszeg alakú, jellemzően anko-val (vörösbab lekvárral) töltött japán édesség. A 20. század vége óta már sokféle  töltelékkel is megtalálható.</p>
+             <p>ízletes édes, lekvár, pudding, csokoládékrém, nutella, gyümölcs felhasználásával édes tésztával</p>
+             <p>Gusztusos, sóstésztában, sajtkrém, hagyma, sonkacsíkok társaságában elkészítve.</p>
+             <p>Az igazi ízletes halacska formájú desszertet, láng fölött sütik!</p>
+        </div>
+         <h3>Készítsd el te is!</h3>
+
+    </div>
+
+
+<div class="ParallaxContent">
+<div class="container">
+<div class="headlines">
+<h3>Recept</h3>
+    <p>
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/sGrZClpDySA" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </p>
+    <p>
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/D4OYNfiReXg" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </p>
+    <p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/MBBwqy3GeQY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </p>
+    <p>
+        <h2>Az anko</h2>
+        avagy vörösbab krém egy igen közkedvelt japán édesség (Igen, ez bizony édesség!), számtalan felhasználási módja van.<br>
+    <span>Hozzávalók:</span>
+    <ul>
+        <li> 200 g azuki bab </li>
+        <li> 100 g cukor </li>
+        <li> csipet só</li>
+    </ul>
+    A babot áztassuk be minimum  egy teljes éjszakára.<br>
+    Öntsük át a babot egy lábasba, és adjunk hozzá még annyi vizet, hogy bőségesen ellepje. <br>
+    Forraljuk fel, majd szűrjük le. Ez elveszi az esetleges kesernyés mellékízt. <br>
+    Töltsük fel újra vízzel, és főzzük addig, ameddig a babszemeket könnyen szét tudjuk nyomni. (Kb. egy óra.)<br>
+    Ha megfőtt a bab, szűrjük le ismét és borítsuk vissza a lábasba. <br>
+    Adjuk hozzá a cukrot és a csipet sót, majd  pépesítsük.
+    </p>
+
+    <p>Az igazi ízletes halacska formájú desszertet, láng fölött sütik!</p>
+</div>
+
+    <div class="ParallaxImage bg1">
+        <div class="container">
+            <h3>Rendeld meg MOST!</h3>
+        </div>
+    </div>
+
+    <div class="ParallaxContent">
+        <div class="container">
+            <div class="headlines">
+                <h3>Nice Effect</h3>
+                <p>All in just one place</p>
+            </div>
+            <p>ízletes édes, lekvár, pudding, csokoládékrém, nutella, gyümölcs felhasználásával édes tésztával</p>
+            <p>Gusztusos, sóstésztában, sajtkrém, hagyma, sonkacsíkok társaságában elkészítve.</p>
+            <p>Az igazi ízletes halacska formájú desszertet, láng fölött sütik!</p>
+            <p>ízletes édes, lekvár, pudding, csokoládékrém, nutella, gyümölcs felhasználásával édes tésztával</p>
+            <p>Gusztusos, sóstésztában, sajtkrém, hagyma, sonkacsíkok társaságában elkészítve.</p>
+            <p>Az igazi ízletes halacska formájú desszertet, láng fölött sütik!</p>
+            <p>ízletes édes, lekvár, pudding, csokoládékrém, nutella, gyümölcs felhasználásával édes tésztával</p>
+            <p>Gusztusos, sóstésztában, sajtkrém, hagyma, sonkacsíkok társaságában elkészítve.</p>
+            <p>Az igazi ízletes halacska formájú desszertet, láng fölött sütik!</p>
+
+        </div>
+    </div>
+
+    <div class="ParallaxImage bg2">
+        <div class="container">
+            <div class="headlines">
+                <h3>Nice Effect</h3>
+                <p>All in just one place</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="ParallaxContent">
+        <div class="container">
+            <div class="headlines">
+                <h3>A taijakit kedvelők között vita van arról, hogy a feje vagy a farka felől érdemes-e enni kezdeni. Gyakran előfordul, ugyanis, hogy a hal farkában már nincs lekvár. Vannak akik szerint a legfinomabb részével, hal lekvárral gazdagon töltött fejével, mások szerint a hal farkával kell kezdeni, mert akkor még az utolsó falatokban is lesz lekvár. </h3>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+</body>
+</html>
+</div>
